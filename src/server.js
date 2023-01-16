91e184768795029cd180a9eef3b7adeb93e9cfa4
김민균 <alsrbs4024@gmail.com>
2023-01-16 16:21:01 +0900

    2023.01.16 middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("home");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here");
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
+  next();
 };
 
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+const handleHome = (req, res) => {
+  return res.send("i love middles");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);

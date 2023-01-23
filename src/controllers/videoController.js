1f01b349b59223b0350d15cf5587486513a49538
김민균 <alsrbs4024@gmail.com>
2023-01-23 15:15:28 +0900

    2023.01.23 pug and partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("Home");
 export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+  return res.render("watch");
 };
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {

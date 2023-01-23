568aa84eb9d36e12b1ba9ea1c72d9d5fa60960fa
김민균 <alsrbs4024@gmail.com>
2023-01-23 20:10:03 +0900

    2023.01.23 templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,8 @@
-export const trending = (req, res) => res.render("Home");
-export const see = (req, res) => {
-  return res.render("watch");
-};
+export const trending = (req, res) => res.render("Home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   return res.send("Delete Video");
 };

cb5b7281478138c02107365b0d91ff7e7dfd3afa
김민균 <alsrbs4024@gmail.com>
2023-01-24 15:33:25 +0900

    2023.01.24 conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,11 @@
-export const trending = (req, res) => res.render("Home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+const fakeUser = {
+  username: "bad",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");

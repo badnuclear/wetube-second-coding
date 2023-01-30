05394cdd87e9ba47c725ff905434751990074a27
김민균 <alsrbs4024@gmail.com>
2023-01-30 14:45:20 +0900

    2023.01.30 mongo DB and connect DB

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/wetube_second");
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log("DB ERROR", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
